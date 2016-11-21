63344a5da4ce4bf531506381ba3f5a8f0499677a
Matías Pardo <mat.pardo@gmail.com>
2016-11-21 04:10:50 -0300

    Aparecer en el Cartesian

---
+++ Assets/Script/Room/Entity/CartesianEntity.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class CartesianEntity : SignalEntity
+{
+
+	protected void Awake(){
+		source  = "warp";
+	}
+	
+	private void nextScene(){
+		SceneLoader.GetInstance ().load("CartesianState");
+	}
+	
+	public override void handleCollision(){
+		base.handleCollision ();
+		Game.GetInstance ().player.wait (3);
+		Invoke ("nextScene", 2);
+	}
+
+	public override void ask ()
+	{
+		SoundManager.instance.PlaySingle ("energia-portal");
+	}
+
+	public override void touch () {
+		SoundManager.instance.PlaySingle ("warp");
+	}
+}
+
